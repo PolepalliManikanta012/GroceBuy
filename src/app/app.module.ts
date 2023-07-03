85c5332795444e4fbe74db477fe361ec0072ff47
P Manikanta <mmani0831@gmail.com>
2023-07-03 10:43:06 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { UpdateprofileComponent } from './updateprofile/updateprofile.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    ItemdetailsComponent,
+    UpdateprofileComponent,
+    OrderlistComponent
   ],
   imports: [
     BrowserModule,
