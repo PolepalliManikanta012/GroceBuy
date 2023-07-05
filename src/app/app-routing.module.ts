28cee19fa22cacd949f523d0af138ec1e64cfc3a
P Manikanta <mmani0831@gmail.com>
2023-07-05 09:09:58 +0530

    Added header,footer,login and registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { UpdateprofileComponent } from './updateprofile/updateprofile.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:HomeComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'registration',component:RegistrationComponent},
+  {path: 'itemdetails',component:ItemdetailsComponent},
+  {path: 'updateprofile',component:UpdateprofileComponent},
+  {path: 'orderlist',component:OrderlistComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
 import { UpdateprofileComponent } from './updateprofile/updateprofile.component';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+
 
 @NgModule({
   declarations: [
@@ -26,7 +31,11 @@ import { OrderlistComponent } from './orderlist/orderlist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
